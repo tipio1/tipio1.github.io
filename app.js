291ea2398734e6a679e8377263589363a5bbc89a
tipio1 <benjamin.rossier@yahoo.fr>
2021-07-01 20:06:12 +0200

    start of the game code in js

---
+++ app.js
@@ -0,0 +1,18 @@
+// Guess The Number => start in July 2021
+let randomNumber = Math.floor(Math.random() * 100) + 1;
+
+let guesses = document.querySelector('.guesses');
+let lastResult = document.querySelector('.lastResult');
+let lowOrHi = document.querySelector('.lowOrHi');
+
+let guessSubmit = document.querySelector('.guessSubmit');
+let guessField = document.querySelector('.guessField');
+
+let guessCount = 1;
+let resetButton;
+
+function checkGuess() {
+    alert('tu va bientôt pouvoir jouer');
+  }
+
+// other
